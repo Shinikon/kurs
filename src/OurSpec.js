36dbf47b6e14650563ded093767bb8b36900fbe9
Shinikon <shinka4u@mail.ru>
2025-03-14 10:57:21 +0500

    Fix: Commit message

--- src/OurSpec.js
+++ src/OurSpec.js
@@ -8,14 +8,14 @@ const data = [
 
   {
     "id": 1,
-    "imageUrl": "%PUBLIC_URL%/assets/img/spec2.svg",
+    "imageUrl": "/kurs/assets/img/spec2.svg",
     "title": "@Vantae",
     "descriptionn": "Универсальный специалист"
   },
 
   {
     "id": 2,
-    "imageUrl": "%PUBLIC_URL%/assets/img/spec3.svg",
+    "imageUrl": "/kurs/assets/img/spec3.svg",
     "title": "@Yumeko",
     "descriptionn": "Геймдизайнер"
   },
