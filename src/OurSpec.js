076a7c8f464bbbad9600280148f5300b5e507fab
Shinikon <shinka4u@mail.ru>
2025-03-14 10:25:01 +0500

    Описание изменений

--- src/OurSpec.js
+++ src/OurSpec.js
@@ -2,21 +2,21 @@
 const data = [
     {
         "id": 0,
-        "imageUrl": "./assets/img/spec1.svg",
+        "imageUrl": "/assets/img/spec1.svg",
         "title": "@Shinka4u",
         "descriptionn": "Архитектурный визуализатор"
       },
 
       {
         "id": 1,
-        "imageUrl": "./assets/img/spec2.svg",
+        "imageUrl": "/assets/img/spec2.svg",
         "title": "@Vantae",
         "descriptionn": "Универсальный специалист"
       },
 
       {
         "id": 2,
-        "imageUrl": "./assets/img/spec3.svg",
+        "imageUrl": "/assets/img/spec3.svg",
         "title": "@Yumeko",
         "descriptionn": "Геймдизайнер"
       },
