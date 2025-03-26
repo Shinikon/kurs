06272a131e5ec990ae45ecfc5a32fb0469ad4317
lgornv <igorkaluta1234567@gmail.com>
2025-03-26 14:54:44 +0500

    адаптация портфолио, исправление макета

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -14,11 +14,11 @@ import slide8 from "../assets/img/slide8.svg";
 import slide9 from "../assets/img/slide9.svg";
 import slide10 from "../assets/img/slide10.svg";
 
-// import slide1 from "../assets/img/slide1.svg";
-// import slide2 from "../assets/img/slide2.svg";
-// import slide3 from "../assets/img/slide3.svg";
-// import slide4 from "../assets/img/slide4.svg";
-// import slide5 from "../assets/img/slide5.svg";
+import slide11 from "../assets/img/slide11.svg";
+import slide12 from "../assets/img/slide12.svg";
+import slide13 from "../assets/img/slide13.svg";
+import slide14 from "../assets/img/slide14.svg";
+import slide15 from "../assets/img/slide15.svg";
 
 import EmblaCarousel from "../components/Carousel";
 import "../scss/components/_carousel.scss";
@@ -28,18 +28,14 @@ const SLIDE_URLS1 = [slide1, slide2, slide3, slide4, slide5];
 
 const SLIDE_URLS2 = [slide6, slide7, slide8, slide9, slide10];
 
+const SLIDE_URLS3 = [slide11, slide12, slide13, slide14, slide15];
+
 function Portfolio() {
   return (
     <div className="wrapper">
       <div className="portfolio">
         <div className="portfolio__title">
-          <h1
-            style={{
-              width: "1072px",
-            }}
-          >
-            Портфолио специалистов Metaform
-          </h1>
+          <h1>Портфолио специалистов Metaform</h1>
         </div>
 
         <div className="portfolio__spec">
@@ -47,7 +43,7 @@ function Portfolio() {
             <div>
               <img src={spec1} />
             </div>
-            <div>
+            <div className="section_desc">
               <h3>@Shinka4u</h3>
               <p>Основное направление: Архитектурная визуализация</p>
               <p>
@@ -66,7 +62,7 @@ function Portfolio() {
             <div>
               <img src={spec2} />
             </div>
-            <div>
+            <div className="section_desc">
               <h3>@Shinka4u</h3>
               <p>Основное направление: Архитектурная визуализация</p>
               <p>
@@ -85,7 +81,7 @@ function Portfolio() {
             <div>
               <img src={spec3} />
             </div>
-            <div>
+            <div className="section_desc">
               <h3>@Shinka4u</h3>
               <p>Основное направление: Архитектурная визуализация</p>
               <p>
@@ -98,7 +94,7 @@ function Portfolio() {
             </div>
           </div>
 
-          {/* СЛАЙДЕР */}
+          <EmblaCarousel slides={SLIDE_URLS3} options={OPTIONS} />
         </div>
       </div>
     </div>
