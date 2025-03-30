73b97bfe1876efa226ad0518015d438760cc46c6
Shinikon <shinka4u@mail.ru>
2025-03-30 14:28:29 +0500

    меню-бургер, адаптация страницы контактов

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,62 @@
-import logoSvg from '../assets/img/logo.svg';
-import '../scss/components/_header.scss';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import logoSvg from "../assets/img/logo.svg";
+import "../scss/components/_header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="header">
       <div className="container">
-        <Link to="/" >
+        <Link to="/">
           <div className="header__logo">
-            <img width="58" height="73" src={logoSvg} alt="logo" />
+            <img src={logoSvg} alt="logo" />
             <div>
               <h2>Vertex Visions</h2>
             </div>
           </div>
         </Link>
-        <ul className="header__nav">
-          <li>
-            <Link to="/" ><h3>Главная</h3></Link>
-          </li>
-          <li>
-            <Link to="/order"><h3>Заказ</h3></Link>
-          </li>
-          <li>
-            <Link to="/portfolio"><h3>Портфолио</h3></Link>
-          </li>
-          <li>
-            <Link to="/contacts"><h3>Контакты</h3></Link>
-          </li>
-        </ul>
+
+        {/* Burger Icon */}
+        <div
+          className={`header__burger ${menuOpen ? "open" : ""}`} // Добавлен класс open
+          onClick={toggleMenu}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        {/* Conditional Rendering of Navigation Menu */}
+        {menuOpen && (
+          <ul className="header__nav">
+            <li>
+              <Link to="/">
+                <h3>Главная</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/order">
+                <h3>Заказ</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/portfolio">
+                <h3>Портфолио</h3>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacts">
+                <h3>Контакты</h3>
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -5,36 +5,36 @@ import ContactForm from "../components/ContactForm";
 
 function Contacts() {
   return (
-    <div className="wrapper">
-      <div className="contacts">
-        <div className="contacts__title">
-          <h1>Контакты</h1>
-        </div>
-        <div className="buttons-animation">
-          <ContactsBtns
-            initialContent="Написать на почту"
-            toggledContent="info@Vertex Visions.studio"
-          />
-          <ContactsBtns
-            initialContent="Написать Вконтакте"
-            toggledContent="info@Vertex Visions.studio"
-          />
-          <ContactsBtns
-            initialContent="Написать в Телеграм"
-            toggledContent="info@Vertex Visions.studio"
-          />
-        </div>
-        <div className="contacts__phone">
-          <h1>+7 (800) 555 35 35</h1>
-          <h1>+8 (800) 355 21 15</h1>
-        </div>
-
-        <div className="contacts__title">
-          <h2>заполните форму</h2>
-        </div>
-        <ContactForm />
+    // <div className="wrapper">
+    <div className="contacts">
+      <div className="contacts__title">
+        <h1>Контакты</h1>
       </div>
+      <div className="buttons-animation">
+        <ContactsBtns
+          initialContent="Написать на почту"
+          toggledContent="info@Vertex Visions.studio"
+        />
+        <ContactsBtns
+          initialContent="Написать Вконтакте"
+          toggledContent="info@Vertex Visions.studio"
+        />
+        <ContactsBtns
+          initialContent="Написать в Телеграм"
+          toggledContent="info@Vertex Visions.studio"
+        />
+      </div>
+      <div className="contacts__phone">
+        <h1>+7 (800) 555 35 35</h1>
+        <h1>+8 (800) 355 21 15</h1>
+      </div>
+
+      <div className="contacts__title">
+        <h2>заполните форму</h2>
+      </div>
+      <ContactForm />
     </div>
+    // </div>
   );
 }
 
